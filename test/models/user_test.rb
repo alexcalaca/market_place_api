99b567253c75a893be5906160dd9eed427b9fc7a
alexcalaca <alexcalaca@gmail.com>
2022-08-26 23:21:19 -0300

    test(user): invalid email test
    Check if a user with an invalid email address is not valid

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,5 +4,10 @@ class UserTest < ActiveSupport::TestCase
   test 'user with a valid email should be valid' do
     user = User.new(email: 'test@test.org', password_digest: 'test')
     assert user.valid?
-  end  
+  end
+
+  test 'user with invalid email should be invalid' do
+    user = User.new(email: 'test', password_digest: 'test')
+    assert_not user.valid?
+  end
 end
