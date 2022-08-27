c3e847e9eb45fb027e037a4fa9c13e168885df5a
alexcalaca <alexcalaca@gmail.com>
2022-08-26 23:30:07 -0300

    test(user): user with a duplicate email test
    Check if a new user with a duplicate email is not valid

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,10 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(email: 'test', password_digest: 'test')
     assert_not user.valid?
   end
+
+  test 'user with taken email should be invalid' do
+    other_user = users(:one)
+    user = User.new(email: other_user.email, password_digest: 'test')
+    assert_not user.valid?
+  end
 end
