5d1dcd07fba53ef5e9e267daa800849c63c269d6
alexcalaca <alexcalaca@gmail.com>
2022-09-03 22:28:33 -0300

    feat(users_controller): add create user action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,20 @@
 class Api::V1::UsersController < ApplicationController
-    def show
-        render json: User.find(params[:id])
-    end
+	def show
+		render json: User.find(params[:id])
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			render json: @user, status: :created # Http status code 201
+		else
+			render json: @user.errors, status: :unprocessable_entity # Http status code 422
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:email, :password)
+	end
 end
