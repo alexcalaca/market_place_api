1b5eb8a662ebf12471d40c8a446d93f626586d01
alexcalaca <alexcalaca@gmail.com>
2022-09-08 00:29:00 -0300

    chore(products_controller): add check owner method

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
 	before_action :set_product, only: %i[show update]
 	before_action :check_login, only: %i[create]
+	before_action :check_owner, only: %i[update]
 	
 	def index
 		render json: Product.all
@@ -24,6 +25,10 @@ class Api::V1::ProductsController < ApplicationController
 	def product_params
 		params.require(:product).permit(:title, :price, :published)
 	end	
+
+	def check_owner
+		head :forbidden unless @product.user_id == current_user&.id
+	end
 
 	def set_product
 		@product = Product.find(params[:id])
