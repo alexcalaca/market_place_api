d7a9115c586303e2b39ecf02e3c122fcebd6bd74
alexcalaca <alexcalaca@gmail.com>
2022-09-10 14:47:51 -0300

    chore(order): total needs to be greater than or equal to 0

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
   belongs_to :user
   
   validates :total, presence: true
-  validates :total, numericality: { greater_than_or_equal_to: 0 }
+  validates :total, numericality: { greater_than_or_equal_to: 0 }  
 end
