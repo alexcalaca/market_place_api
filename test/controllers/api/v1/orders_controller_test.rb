b12f49af2670f2aa0988e9e7897de0ae7a82f7d4
alexcalaca <alexcalaca@gmail.com>
2022-09-11 12:31:11 -0300

    test(orders_controller_test): add test 'forbid unlogged users to access orders'

--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -5,4 +5,9 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
     @order = orders(:one)
   end
 
+  test 'should forbid orders for unlogged' do
+    get api_v1_orders_url, as: :json
+    assert_response :forbidden #The 403 (Forbidden) status code indicates that the server understood the request but refuses to authorize it
+  end
+
 end
