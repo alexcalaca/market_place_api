55255a138ebb14caf9ef1bb83fddca08e08f6847
alexcalaca <alexcalaca@gmail.com>
2022-09-12 00:34:01 -0300

    test(orders_controller_test): add test 'show orders'

--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -10,4 +10,13 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_response :forbidden #The 403 (Forbidden) status code indicates that the server understood the request but refuses to authorize it
   end
 
+  test 'should show orders' do
+    get api_v1_orders_url,
+      headers: { Authorization: JsonWebToken.encode(user_id: @order.user_id) },
+      as: :json
+    assert_response :success
+
+    json_response = JSON.parse(response.body)
+    assert_equal @order.user.orders.count, json_response['data'].count
+  end
 end
