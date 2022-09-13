20a484440f20166f17a031e2e7e8af2fc0e7253c
alexcalaca <alexcalaca@gmail.com>
2022-09-13 15:07:15 -0300

    feat(orders_controller): add create action
    Create order based on current user logged

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -15,4 +15,20 @@ class Api::V1::OrdersController < ApplicationController
 			head 404
 		end		
 	end
+
+	def create
+		order = current_user.orders.build(order_params)
+
+		if order.save
+			render json: order, status: 201 # Created
+		else
+			render json: { errors: order.errors }, status: 422 # Unprocessable Entity
+		end		
+	end
+
+	private
+
+	def order_params
+		params.require(:order).permit(:total, product_ids: [])
+	end
 end
