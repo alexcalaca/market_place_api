5e9f8c316b89f0e866d9c9b11b93f6d0222642b5
alexcalaca <alexcalaca@gmail.com>
2022-09-13 19:17:02 -0300

    test(order_test): add initial setup for order test

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
-class OrderTest < ActiveSupport::TestCase
-  test 'should have a positive total' do
-    order = orders(:one)
-    order.total = -1
-    assert_not order.valid?
-  end
+class OrderTest < ActiveSupport::TestCase  
+    setup do
+      @order = orders(:one)
+      @product1 = products(:one)
+      @product2 = products(:two)
+    end
 end
