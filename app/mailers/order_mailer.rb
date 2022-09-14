0c6d55823aa84859fc26492f15f17afb12678d2b
alexcalaca <alexcalaca@gmail.com>
2022-09-13 22:55:28 -0300

    feat(order_mailer): create 'send confirmation' method

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,13 +1,10 @@
 class OrderMailer < ApplicationMailer
+  default from: 'no-reply@marketplace.com'
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.order_mailer.send_confirmation.subject
-  #
-  def send_confirmation
-    @greeting = "Hi"
+  def send_confirmation(order)
+    @order = order
+    @user = @order.user
 
-    mail to: "to@example.org"
+    mail to: @user.email, subject: 'Order confirmation'
   end
 end
