eb639e6c141c967505543b5120c16322c2899a77
alexcalaca <alexcalaca@gmail.com>
2022-09-16 16:22:15 -0300

    feat(placement): decrement product quantity based on placement quantity

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,4 +1,9 @@
 class Placement < ApplicationRecord
+
   belongs_to :order
-  belongs_to :product, inverse_of: :placements
+  belongs_to :product, inverse_of: :placements  
+
+  def decrement_product_quantity!
+    product.decrement!(:quantity, quantity)
+  end
 end
